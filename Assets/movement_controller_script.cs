0bf94907e1643ae9901366bd5e6f07d178e3f40e
Anderson <castilhos_cmpa@hotmail.com>
2018-11-12 16:19:04 -0200

    Espelho plano lvl 1 funcional

---
+++ Assets/movement_controller_script.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement_controller_script : MonoBehaviour {
+
+	public GameObject objReal;
+	public GameObject objImagem;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Movement(int direction){
+		const float offset = 0.5f;
+		const int esquerda = -1;
+
+		if (direction == esquerda)    
+			objReal.transform.localPosition = new Vector3(objReal.transform.localPosition.x - offset,objReal.transform.localPosition.y,objReal.transform.localPosition.z);
+		else 					//DIREITA
+			objReal.transform.localPosition =  new Vector3(objReal.transform.localPosition.x + offset,objReal.transform.localPosition.y,objReal.transform.localPosition.z);
+
+
+
+	}
+
+}
---
+++ Assets/plano_script.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System;
+using UnityEngine.UI;
+
+public class plano_script : MonoBehaviour
+{
+	private float translationSpeed = 2;
+	bool repeatPositionLeft = false;
+	bool repeatPositionRight = false;
+
+	public GameObject objReal;
+	public GameObject objImagem;
+
+
+
+	void Start()
+	{
+		
+	}
+		
+	void Update ()
+	{
+
+		if (repeatPositionLeft) {
+			PositionLeftButton();
+		}
+
+		if (repeatPositionRight) {
+			PositionRightButton();
+		}
+
+	}
+
+	public void PositionLeftButtonRepeat ()
+	{
+		repeatPositionLeft = true;
+	}
+
+	public void PositionRightButtonRepeat ()
+	{
+		repeatPositionRight = true;
+	}
+
+
+	public void PositionLeftButtonOff ()
+	{
+		repeatPositionLeft = false;
+	}
+
+	public void PositionRightButtonOff ()
+	{
+		repeatPositionRight = false;
+	}
+
+
+	public void PositionRightButton ()
+	{
+		const float boundaryRightLimit = 3.82f; 
+		//Debug.Log (objReal.transform.localPosition.x);
+		if (objReal.transform.localPosition.x < boundaryRightLimit) {
+			
+			objReal.transform.Translate (translationSpeed * Time.deltaTime, 0, 0);
+			objImagem.transform.Translate (-translationSpeed * Time.deltaTime, 0, 0);
+		}
+	}
+
+	public void PositionLeftButton ()
+	{
+		const float boundaryLeftLimit = 1.42f; 
+
+		if (objReal.transform.localPosition.x > boundaryLeftLimit) {
+			objReal.transform.Translate (-translationSpeed * Time.deltaTime, 0, 0);
+			objImagem.transform.Translate (translationSpeed * Time.deltaTime, 0, 0);
+		}
+	}
+
+}
